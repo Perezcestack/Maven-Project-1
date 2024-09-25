341975fc8a5df4ce913b261b149169df8fcf7c68
Cesar <Perezce41@gmail.com>
2024-09-25 11:15:55 -0700

    first commit

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("apple");
+        System.out.println("apple");
+        System.out.println("apple");
+        System.out.println("apple");
+        System.out.println("apple");
+        System.out.println("apple");
+        System.out.println("apple");
+    }
+}
